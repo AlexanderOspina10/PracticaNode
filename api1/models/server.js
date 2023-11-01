1c0426a5d6dcb475f1268a20f59c2f5c7097ddb2
AlexanderOspina10 <ospinahincapie10@gmail.com>
2023-11-01 11:08:17 -0500

    NOVIMEBRE

--- api1/models/server.js
+++ api1/models/server.js
@@ -44,11 +44,28 @@
             this.app.get('/consultarAprendiz', (req, res) => {
 
                 const {documento, nombre} = req.query // Recibir los parametros
+                let resultado = ''
+                //Buscar si el documento recibido existe en el array de aprendicez
+                resultado = aprendices.find(aprendiz => aprendiz.documento === parseInt(documento))
 
                 res.json({
-                    msg:documento
+                    msg:resultado
                 })
             })
+
+
+            //Metodo agregar elementos al array 
+            this.app.post('/',(req, res) => {
+                const {documento, nombre, apellidos} = req.query
+                aprendices.push({
+                    "documento":parseInt(documento),
+                    "nombre":nombre,
+                    "apellidos":apellidos
+                })
+                res.json({
+                    msg:aprendices
+                })
+            } )
 
         }
     }
